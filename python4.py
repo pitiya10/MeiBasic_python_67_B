fc6cb12cb275f0075d9c53c54f0475b7ef9b87d8
Pitiya <6749010036@phuketvc.ac.th>
2024-08-18 14:25:19 +0700

    python python4.py

---
+++ python4.py
@@ -0,0 +1,50 @@
+"""
+#
+# Part : Python Condition
+#
+"""
+a =  200
+b = 500
+if a < b:
+    print("a < b is ture")
+    print("a < b is ture")
+    print("a < b is ture")
+elif a > b:
+    print("a > b is ture")
+elif a == b:
+    print("a == b is ture")
+elif a >= b:
+    print("a >= b is ture")
+else:
+    print("์Nothing")
+
+if a > b: print("a > b is True")
+print("A") if a > b else print("B") #แบบย่อ
+
+a = 200
+b = 50
+c = 500
+if(a > b)and(c > a):
+    print("both is ture")
+if a > b or a > c:
+    print("Some cond is Ture")
+if not (a > b):
+ print("False")
+if a > b:
+     pass
+ 
+x = 23
+if x > 10:
+    print("Let's go")
+    if x > 20:
+        print("x > 20 is Ture")
+        if x > 20:
+         print("x > 20 is Ture") 
+         if x > 20:
+            print("x > 20 is Ture")
+            if x > 20:
+             print("x > 20 is Ture")
+
+
+        
+        
